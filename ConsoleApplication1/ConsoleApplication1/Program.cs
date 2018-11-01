e268d165311bb6a58bbe9f9a839d4d4b9a965f1b
karalapativ <karalapati@gmail.com>
2018-11-01 13:24:19 +0530

    dsfdsf

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -19,6 +19,8 @@ namespace ConsoleApplication1
             Console.WriteLine("Hello TFS13");
 
             Console.WriteLine("Hello GIT");
+            Console.WriteLine("Hello GI12T");
+
         }
     }
 }
